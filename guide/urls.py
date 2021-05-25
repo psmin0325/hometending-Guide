ceb6ca909592a8affa2734a4cb99d7177ed5e411
박상민 <psmin9325@gmail.com>
2021-05-25 23:56:58 +0900

    2일차 완료

--- guide/urls.py
+++ guide/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name='guide'
+
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='index'),
+    path('support/', views.support, name='support'),
 ]
--- guide/views.py
+++ guide/views.py
@@ -7,10 +7,12 @@ def index(request):
     """
     기주 목록 출력
     """
-    baseAlchohol_list = baseAlchohol.img
     #return HttpResponse("안녕하세요. 홈텐딩 길라잡이에 오신것을 환영합니다.")
     return render(request, 'guide/baseAlchohol_list.html')
-#     question_list = Question.objects.order_by('-create_date')
-#     context = {'question_list': question_list}
-# # ---------------------------------- [edit] ---------------------------------- #
-#     return render(request, 'pybo/question_list.js', context)
+
+def support(request):
+    """
+    support 목록 출력
+    """
+    return render(request, 'guide/secondAlchohol_list.html')
+
