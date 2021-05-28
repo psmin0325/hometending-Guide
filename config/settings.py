b44141cf6c5b2d6640e35215dc581ddefaf6e90c
박상민 <psmin9325@gmail.com>
2021-05-28 23:45:29 +0900

    3일차

--- config/settings.py
+++ config/settings.py
@@ -123,6 +123,3 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'hometending', 'static')
-# ]
--- guide/views.py
+++ guide/views.py
@@ -11,23 +11,66 @@ def index(request):
     """
     기주 목록 출력
     """
-    return render(request, 'guide/baseAlchohol_list.html')
+    baseAlchol = {"vodka": 0, "rum": 1}
+    return render(request, 'guide/baseAlchohol_list.html', baseAlchol)
     #return HttpResponse("안녕하세요. 홈텐딩 길라잡이에 오신것을 환영합니다.")
 
-def support(request):
+def vodka(request):
     """
-    support 목록 출력
+    support_vodka 목록 출력
     """
-    return render(request, 'guide/secondAlchohol_list.html')
+    return render(request, 'guide/support/vodka.html')
 
-def additives(request):
+def v_orange(request):
     """
-    첨가물 목록 출력
+    v_orange 목록 출력
     """
-    return render(request, 'guide/additives_list.html')
+    return render(request, 'guide/additives/v_orange.html')
 
-def result(request):
+def voi_result(request):
     """
     결과 화면 출력
     """
-    return render(request, 'guide/result.html')
+    return render(request, 'guide/result/voiResult.html')
+
+def v_cranberry(request):
+    """
+    v_orange 목록 출력
+    """
+    return render(request, 'guide/additives/v_cranberry.html')
+
+def voc_result(request):
+    """
+    결과 화면 출력
+    """
+    return render(request, 'guide/result/vocResult.html')
+
+def rum(request):
+    """
+    rum 목록 출력
+    """
+    return render(request, 'guide/support/rum.html')
+
+def r_orange(request):
+    """
+    r_orange 목록 출력
+    """
+    return render(request, 'guide/additives/r_orange.html')
+
+def roi_result(request):
+    """
+    r_orange 목록 출력
+    """
+    return render(request, 'guide/result/roiResult.html')
+
+def r_cola(request):
+    """
+    r_orange 목록 출력
+    """
+    return render(request, 'guide/additives/r_cola.html')
+
+def rcl_result(request):
+    """
+    r_orange 목록 출력
+    """
+    return render(request, 'guide/result/rclResult.html')
